2586f671e278a92d7b35397238e14d840266f902
light1331 <lightyagami80262@gmail.com>
2025-10-21 02:07:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -219,7 +219,7 @@ gsap.from(".page3Head>small", {
 });
 
 gsap.to(".page3 .slider h1", {
-  transform: 'translateX(-80%)',
+  transform: 'translateX(-81%)',
   scrollTrigger: {
     trigger: ".page3",
     scroller: "body",
